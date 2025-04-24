89d121f19fad5989dc42f86f4fc6e85006b4b948
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-04-24 11:21:39 +0000

    hibernate not working

--- Section-6-Hibernate/HibernateDemo/src/main/java/org/example/Main.java
+++ Section-6-Hibernate/HibernateDemo/src/main/java/org/example/Main.java
@@ -1,8 +1,28 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        Student s1=new Student();
+        Student s1 = new Student();
+        s1.setSid(6);
+        s1.setSname("Ussop");
+        s1.setSmarks(75);
+
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(org.example.Student.class);
+        cfg.configure("hibernate.cfg.xml");
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction transaction=session.beginTransaction();
+
+        session.save(s1);
+
+        transaction.commit();
         System.out.println(s1);
     }
 }
