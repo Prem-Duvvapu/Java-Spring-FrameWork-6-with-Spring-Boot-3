2461a67b555e81d7eb101474999b6a7c0da9fe9f
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-04-29 19:55:19 +0000

    REST using spring boot

---
+++ Section-15-REST-using-Spring-Boot/SpringBootREST/src/main/java/com/example/SpringBootREST/repo/JobRepo.java
@@ -0,0 +1,35 @@
+package com.example.SpringBootREST.repo;
+
+import com.example.SpringBootREST.model.JobPost;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class JobRepo {
+
+    List<JobPost> jobs=new ArrayList<>(Arrays.asList(
+            new JobPost(1,"Java Developer","experience in core Java",1,new ArrayList<>(Arrays.asList("Java","Spring","Spring Boot"))),
+            new JobPost(2,"React Developer","experience in JS",2,new ArrayList<>(Arrays.asList("Java Script","React"))),
+            new JobPost(3,"Python Developer","experience in DSA",2,new ArrayList<>(Arrays.asList("Python","DSA")))
+    ));
+
+    public List<JobPost> getAllJobs() {
+        return jobs;
+    }
+
+    public void addJob(JobPost jobPost) {
+        jobs.add(jobPost);
+        System.out.println(jobs);
+    }
+
+    public JobPost getJob(int postId) {
+        for (JobPost job: jobs)
+            if (job.getPostId() == postId)
+                return job;
+
+        return null;
+    }
+}
